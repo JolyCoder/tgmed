9e401c97875aae5c7eed614a821683cd5b43c448
Ilya Tvaliashvili <tvaliaschvili1234@gmail.com>
2019-11-10 01:35:29 +0300

    Bugs fixed

--- models/schedule.js
+++ models/schedule.js
@@ -1,13 +1,13 @@
 const db = require("../parts/db")
 
 exports.addSchedule = (schedule, cb) => {
-	db.get().collection("schedules").insertOne(schedule, (err, result) => {
+	db.get().collection("schedule").insertOne(schedule, (err, result) => {
 		cb(err, result);
 	});
 };
 
 exports.getSchedules = (cb) => {
-	db.get().collection("schedules").find().toArray((err, docs) => {
+	db.get().collection("schedule").find().toArray((err, docs) => {
 		cb(err, docs);
 	});
 };
