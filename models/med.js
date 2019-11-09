9f8e11d2ab67658b73c58156fc593e66560e25ac
Ilya Tvaliashvili <tvaliaschvili1234@gmail.com>
2019-11-09 16:04:56 +0300

    Bugs fixed

--- models/med.js
+++ models/med.js
@@ -3,5 +3,11 @@ const db = require("../parts/db")
 exports.addMed_model = (med, cb) => {
 	db.get().collection("meds").insert(med, (err, result) => {
 		cb(err, result);
-	})
+	});
+};
+
+exports.getMed_model = (cb) => {
+	db.get().collection("meds").find().toArray((err, result) => {
+		cb(err, result);
+	});
 };
