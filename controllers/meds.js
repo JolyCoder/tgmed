9b551ec754510ffdfd23e37ca6240d39a5f9fabd
Ilya Tvaliashvili <tvaliaschvili1234@gmail.com>
2019-11-09 16:36:03 +0300

    Bugs added

--- controllers/meds.js
+++ controllers/meds.js
@@ -3,8 +3,8 @@ const medsModel = require("../models/med")
 exports.addMed_Controller = (req, res) => {
 	medsModel.addMed_model(req.body, (err, result) => {
 		if(err)
-			return res.sendStatus(500);
+			return res.sendStatus(500) && console.log(err);
 		else
-			return res.sendStatus(200);
+			return res.sendStatus(200) && console.log(err);
 	})	
 };
