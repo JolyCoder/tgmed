7e099b4583edf6a01089f9281a3785032af889de
Ilya Tvaliashvili <tvaliaschvili1234@gmail.com>
2019-11-10 01:54:27 +0300

    Code format

--- models/med.js
+++ models/med.js
@@ -1,24 +1,24 @@
 const db = require("../parts/db")
 
 exports.addMed_model = (med, cb) => {
-	db.get().collection("meds").insert(med, (err, result) => {
-		cb(err, result);
-	});
+  db.get().collection("meds").insert(med, (err, result) => {
+    cb(err, result);
+  });
 };
 
 exports.getMed_model = (cb) => {
-	db.get().collection("meds").find().toArray((err, result) => {
-		cb(err, result);
-	});
+  db.get().collection("meds").find().toArray((err, result) => {
+    cb(err, result);
+  });
 };
 
 exports.getMedbyNumber_model = (num, cb) => {
-	db.get().collection("meds").find().toArray((err, result) => {
-		for(var clinic of result) {
-			if(clinic.num == num) {
-				return cb(err, clinic);
-			}
-		}
-	});
-	return cb(err, "not find");
+  db.get().collection("meds").find().toArray((err, result) => {
+    for (var clinic of result) {
+      if (clinic.num == num) {
+        return cb(err, clinic);
+      }
+    }
+  });
+  return cb(err, "not find");
 };
