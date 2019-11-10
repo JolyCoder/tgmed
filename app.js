f7354540edd6624679b7aa6a416f9fae7a05173e
Ilya Tvaliashvili <tvaliaschvili1234@gmail.com>
2019-11-10 14:23:44 +0300

    Bug added

--- app.js
+++ app.js
@@ -196,6 +196,8 @@ db.connect(config.mongouri, config.mogoname, (err) => {
 					for(var priem of docs) {
 						if(priem.idDoct == msg.chat.id) {
 							cardModel.getCardByID(priem.idUser, (err, card) => {
+								if(err)
+									return;
 								console.log(card);
 								message += priem.time + " числа с " + card.name + ", ";
 							});
