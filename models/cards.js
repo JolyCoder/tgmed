1830bd8591a1ea039fb31833dbf001fa426bb1df
Ilya Tvaliashvili <tvaliaschvili1234@gmail.com>
2019-11-10 14:17:11 +0300

    LOOL BUG NOT FIXED AGAIN

--- models/cards.js
+++ models/cards.js
@@ -3,6 +3,7 @@ const db = require("../parts/db")
 exports.getCardByID = (id, cb) => {
 	db.get().collection("cards").find().toArray((err, docs) => {
 		for(var card of docs) {
+			console.log("card", card)
 			if(card.id == id) {
 				return cb(err, card);
 			}
